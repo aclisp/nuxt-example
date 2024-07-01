f903b579b5efe0be19322205deb3f9eb61fbdc6a
huanghao <huanghao@youxin.cloud>
2024-07-01 09:28:56 +0800

    add emailjs

---
+++ server/api/sendmail2.ts
@@ -0,0 +1,27 @@
+import { SMTPClient } from 'emailjs'
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig(event)
+
+  const mailer = new SMTPClient({
+    user: config.sendmailUser,
+    password: config.sendmailPass,
+    host: 'smtp.qq.com',
+    port: 465,
+    ssl: true,
+  })
+
+  const body = await readBody(event)
+  const { to, subject, text } = body
+
+  try {
+    await mailer.sendAsync({
+      from: config.sendmailUser,
+      to, subject, text,
+    })
+  }
+  catch (e) {
+    return { error: String(e) }
+  }
+  return { ok: true }
+})
